dcd82bf4333e1cda56c6e0785d0287cc959ade5c
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2026-02-23 01:45:58 +0330

    trying to init superuser

--- scripts/init_superuser.py
+++ scripts/init_superuser.py
@@ -1,62 +1,53 @@
 import logging
+from uuid import UUID
+import asyncio
 
-from pydantic import BaseModel
-from src.models.dtos.auth.domain.v1.auth_domain_interface_dtos import RegisterUserInputDTOV1
-from src.utils.security_utils import SecurityUtils
+from archipy.adapters.postgres.sqlalchemy.adapters import AsyncPostgresSQLAlchemyAdapter
+from archipy.models.entities.sqlalchemy.base_entities import BaseEntity
+from archipy.models.errors import DatabaseQueryError, DatabaseConnectionError
+from sqlalchemy import Column, String
+from src.configs.runtime_config import RuntimeConfig
 from src.models.entities.user_entity import UserEntity
-from sqlalchemy import select
+from src.utils.security_utils import SecurityUtils
+
+configs = RuntimeConfig.global_config()
+from src.models.dtos.auth.domain.v1.auth_domain_interface_dtos import RegisterUserInputDTOV1
 
 
-class SuperUserCreateDTO(RegisterUserInputDTOV1):
-    is_superuser: bool = True
-
-
-def init_db(session) -> None:
-    # load these values from .env
-    # first_super_user_email
-    # first_super_user_first_name
-    # first_super_user_last_name
-    # first_super_user_username
-    # first_super_user_password
-    (
-        first_super_user_email,
-        first_super_user_first_name,
-        first_super_user_last_name,
-        first_super_user_username,
-        first_super_user_password,
-    ) = (None, None, None, None, None)
-    user = session.exec(select(UserEntity).where(UserEntity.email == first_super_user_email)).first()
-    if not user:
-        user_in = SuperUserCreateDTO(
-            email=first_super_user_email,
-            first_name=first_super_user_first_name,
-            last_name=first_super_user_last_name,
-            username=first_super_user_username,
-            password=first_super_user_password,
-            is_superuser=True,
-        )
-        db_obj = UserEntity(
-            **user_in.model_dump(), update={"hashed_password": SecurityUtils.get_password_hash(user_in.password)}
-        )
-        session.add(db_obj)
-        session.commit()
-        session.refresh(db_obj)
-
-
-logging.basicConfig(level=logging.INFO)
+# Configure logging
 logger = logging.getLogger(__name__)
 
+class SuperUserCreateDTO(RegisterUserInputDTOV1):
+    is_super_user: bool = True
 
-def init() -> None:
-    with Session(engine) as session:
-        init_db(session)
+# Create adapter
+async def init_super_user():
+    adapter = AsyncPostgresSQLAlchemyAdapter()
+    try:
+        session = adapter.get_session()
 
+        user_in = SuperUserCreateDTO(
+            email=configs.FIRST_SUPERUSER_EMAIL,
+            first_name=configs.FIRST_SUPERUSER_FIRSTNAME,
+            last_name=configs.FIRST_SUPERUSER_LASTNAME,
+            username=configs.FIRST_SUPERUSER_USERNAME,
+            password=configs.FIRST_SUPERUSER_PASSWORD,
+            is_super_user=True,
+        )
+        user_in_password = user_in.password
+        del user_in.password
+        db_obj = UserEntity(
+            **{
+                **user_in.model_dump(),
+                "hashed_password": SecurityUtils.get_password_hash(user_in_password)
+            }
+        )
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
 
-def main() -> None:
-    logger.info("Creating initial data")
-    init()
-    logger.info("Initial data created")
+    except (DatabaseQueryError, DatabaseConnectionError) as e:
+        logger.error(f"Database operation failed: {e}")
+        raise
 
-
-if __name__ == "__main__":
-    main()
+asyncio.run(init_super_user())
--- src/configs/runtime_config.py
+++ src/configs/runtime_config.py
@@ -1,8 +1,12 @@
 from archipy.configs.base_config import BaseConfig
+from pydantic import EmailStr
 
 
 class RuntimeConfig(BaseConfig):
-    FIRST_SUPERUSER: str
+    FIRST_SUPERUSER_EMAIL: EmailStr
+    FIRST_SUPERUSER_FIRSTNAME: str
+    FIRST_SUPERUSER_LASTNAME: str
+    FIRST_SUPERUSER_USERNAME: str
     FIRST_SUPERUSER_PASSWORD: str
 
 
